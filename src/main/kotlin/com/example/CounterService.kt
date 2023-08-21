ec0b545cb4e8ccad5b7ce9da25446873142aac0d
Alessio Coser <>
2023-08-21 11:30:00 +0200

    remove properties magic

--- src/main/kotlin/com/example/CounterService.kt
+++ src/main/kotlin/com/example/CounterService.kt
@@ -1,15 +1,11 @@
 package com.example
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.concurrent.atomic.AtomicLong
 
 @Service
-class CounterService {
-
-    @Value("#{new Integer('\${example.initial}')}")
-    private val initial: Int? = null
-    private val atomic: AtomicLong = AtomicLong((initial ?: 0).toLong())
+class CounterService(props: Properties) {
+    private val atomic = AtomicLong(props.exampleInitial.toLong())
 
     fun incrementAndGet(): Long {
         return atomic.incrementAndGet()

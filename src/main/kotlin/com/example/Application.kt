56bd24c2fe517d59068a06767c07d793d1177d52
Alessio Coser <>
2023-08-23 10:45:52 +0200

    move the routing to the run application context

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,17 +3,13 @@ package com.example
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.function.RouterFunction
-import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.context.support.beans
 import org.springframework.web.servlet.function.router
 
 fun main(args: Array<String>) {
     Application().start(args)
 }
 
-@Configuration
 @SpringBootApplication
 class Application: AutoCloseable {
     private val props: Properties = Properties()
@@ -21,13 +17,13 @@ class Application: AutoCloseable {
     private var appContext: ConfigurableApplicationContext? = null
     private val app = SpringApplication(Application::class.java).apply {
         setDefaultProperties(mapOf("server.port" to props.serverPort))
-    }
-
-    @Bean
-    fun router(): RouterFunction<ServerResponse> {
-        return router {
-            config.greetingController.routes(this)
-        }
+        addInitializers(beans {
+            bean {
+                router {
+                    config.greetingController.routes(this)
+                }
+            }
+        })
     }
 
     fun start(args: Array<String>): Application {

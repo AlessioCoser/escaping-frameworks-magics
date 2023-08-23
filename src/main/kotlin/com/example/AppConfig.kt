232919f8710211d2c0af7223fb423bc38b13c133
Alessio Coser <>
2023-08-23 11:50:15 +0200

    remove the not-needed @Configuration annotation

--- src/main/kotlin/com/example/AppConfig.kt
+++ src/main/kotlin/com/example/AppConfig.kt
@@ -1,15 +1,8 @@
 package com.example
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
 class AppConfig {
-
-    @Bean
     fun properties() = Properties()
 
-    @Bean
     fun counterService(props: Properties) = CounterService(props.exampleInitial)
 
     fun greetingController() = GreetingController(counterService(properties()))

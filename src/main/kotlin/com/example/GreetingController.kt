4889be28b225f187dc3863f2fc7cee122194bd78
Alessio Coser <>
2023-08-24 12:27:04 +0200

    remove @ResponseBody annotation and use ResponseEntity

--- src/main/kotlin/com/example/GreetingController.kt
+++ src/main/kotlin/com/example/GreetingController.kt
@@ -2,19 +2,19 @@ package com.example
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @RequestMapping("greeting")
 class GreetingController(private val counterService: CounterService) {
 
     @GetMapping
-    @ResponseBody
-    fun greeting(request: HttpServletRequest, response: HttpServletResponse): Greeting {
+    fun greeting(request: HttpServletRequest, response: HttpServletResponse): ResponseEntity<Greeting> {
         val name = request.getParameter("name") ?: "World"
-        return Greeting(counterService.incrementAndGet(), "Hello, ${name}!")
+        return ok().body(Greeting(counterService.incrementAndGet(), "Hello, ${name}!"))
     }
 }

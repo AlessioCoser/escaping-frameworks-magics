ebae80abb0b0e883191b9fe84e260b2157f64448
Alessio Coser <>
2023-08-24 12:20:10 +0200

    remove @RestController magic and use @Controller with @RequestBody explicitly

--- src/main/kotlin/com/example/GreetingController.kt
+++ src/main/kotlin/com/example/GreetingController.kt
@@ -2,15 +2,17 @@ package com.example
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ResponseBody
 
-@RestController
+@Controller
 @RequestMapping("greeting")
 class GreetingController(private val counterService: CounterService) {
 
     @GetMapping
+    @ResponseBody
     fun greeting(request: HttpServletRequest, response: HttpServletResponse): Greeting {
         val name = request.getParameter("name") ?: "World"
         return Greeting(counterService.incrementAndGet(), "Hello, ${name}!")

52a6b471af69094dd6ef69622e7994065eccc511
Alessio Coser <>
2023-08-24 11:59:43 +0200

    remove @Controller and @GetMapping magics

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,10 +2,23 @@ package com.example
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.function.RouterFunction
+import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.router
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
 }
 
+@Configuration
 @SpringBootApplication
-class Application
+class Application {
+    @Bean
+    fun router(greetingController: GreetingController): RouterFunction<ServerResponse> {
+        return router {
+            GET("/greeting") { greetingController.greeting(it) }
+        }
+    }
+}
--- src/main/kotlin/com/example/GreetingController.kt
+++ src/main/kotlin/com/example/GreetingController.kt
@@ -1,20 +1,15 @@
 package com.example
 
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.ok
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.ServerResponse.ok
 
-@Controller
-@RequestMapping("greeting")
+@Component
 class GreetingController(private val counterService: CounterService) {
 
-    @GetMapping
-    fun greeting(request: HttpServletRequest, response: HttpServletResponse): ResponseEntity<Greeting> {
-        val name = request.getParameter("name") ?: "World"
+    fun greeting(request: ServerRequest): ServerResponse {
+        val name = request.param("name").orElse("World")
         return ok().body(Greeting(counterService.incrementAndGet(), "Hello, ${name}!"))
     }
 }

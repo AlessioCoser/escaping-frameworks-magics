a365cb9ebb6d655c88352575f80f1601041f9136
Alessio Coser <>
2023-10-04 09:56:25 +0200

    start the real application in tests

--- src/test/kotlin/com/example/GreetingControllerTests.kt
+++ src/test/kotlin/com/example/GreetingControllerTests.kt
@@ -2,32 +2,42 @@ package com.example
 
 import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.junit.jupiter.api.TestInstance
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestTemplate
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GreetingControllerTests {
-    @Autowired
-    private val mockMvc: MockMvc? = null
+    private val application = Application()
 
     @Test
     fun noParamGreetingShouldReturnDefaultMessage() {
-        val response = mockMvc!!.perform(get("/greeting")).andReturn().response
-        assertThat(response.status).isEqualTo(200)
-        assertThatJson(response.contentAsString).inPath("$.content").isEqualTo("Hello, World!")
+        val response = get("/greeting")
+        assertThat(response.statusCode.value()).isEqualTo(200)
+        assertThatJson(response.body).inPath("$.content").isEqualTo("Hello, World!")
     }
 
     @Test
     fun paramGreetingShouldReturnTailoredMessage() {
-        val response = mockMvc!!.perform(
-            get("/greeting").param("name", "Spring Community")
-        ).andReturn().response
-        assertThat(response.status).isEqualTo(200)
-        assertThatJson(response.contentAsString).inPath("$.content").isEqualTo("Hello, Spring Community!")
+        val response = get("/greeting?name=Spring Community")
+        assertThat(response.statusCode.value()).isEqualTo(200)
+        assertThatJson(response.body).inPath("$.content").isEqualTo("Hello, Spring Community!")
+    }
+
+    @BeforeAll
+    fun beforeAll() {
+        application.start(emptyArray())
+    }
+
+    @AfterAll
+    fun afterAll() {
+        application.stop()
+    }
+
+    private fun get(uri: String): ResponseEntity<String> {
+        return RestTemplate().getForEntity("http://localhost:8080$uri", String::class.java)
     }
 }
